f4e72558c727212c2b11235d5c15f62c4e9ac3ed
trishi-ri <trishi-ri@ya.ru>
2022-10-28 00:28:04 +0300

    gradient-emotion variant

--- pages/emotions.js
+++ pages/emotions.js
@@ -1,5 +1,15 @@
-if (weather.classList.contains("rain")) {
-  object.classList.add("sad");
-} else {
-  object.classList.remove("sad");
-}
+const objectGradient = document.querySelector("#ObjectGradient");
+
+const sadEmotion = "sad";
+
+const checkSad = () => {
+  if (weather.classList.contains("rain")) {
+    object.classList.add(sadEmotion);
+    objectGradient.classList.add(sadEmotion);
+  } else {
+    object.classList.remove(sadEmotion);
+    objectGradient.classList.remove(sadEmotion);
+  }
+};
+
+setInterval(checkSad, 1000);
